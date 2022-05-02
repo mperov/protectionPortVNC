4e8a8ec007276e953c3fe6a3497c04d9440a60f5
Maksim Perov <coder@frtk.ru>
2022-05-02 15:53:51 +0300

    Update scannerServices.sh

--- scannerServices.sh
+++ scannerServices.sh
@@ -4,7 +4,9 @@
 # Copyright (c) 2022 Maksim Perov <coder@frtk.ru>
 #
 
-VNC_SRV=`netstat -tulpn 2>/dev/null | grep 0:59 | awk '{print $7}'`
+PATTERN="0:59"
+
+VNC_SRV=`netstat -tulpn 2>/dev/null | grep $PATTERN | awk '{print $7}'`
 
 for srv in $VNC_SRV; do
   echo $srv
